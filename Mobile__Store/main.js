8ef89fbf501d858ca7095c58e03f7b3a04ffe18f
thanghoang <hoanganhthang357@gmail.com>
2021-09-19 11:11:49 +0700

    Add JS in location--modal header and Create footer

---
+++ Mobile__Store/main.js
@@ -0,0 +1,20 @@
+// Header Location Choose
+var $ = document.querySelector.bind(document)
+var $$ = document.querySelectorAll.bind(document)
+
+var locationSelect = $('.header__location-select-primary')
+var locationItem = $$('.header__location-modal-select')
+var locationModal = $('.header__location-modal')
+
+function getLocation() {
+    for(var i = 0; i < locationItem.length; i++) {
+        locationItem[i].onclick = function() {
+            locationSelect.innerHTML = `${this.innerHTML}`
+        }
+    }
+}
+
+getLocation()
+ 
+
+
